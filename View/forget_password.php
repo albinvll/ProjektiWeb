6e2f1c2b253a811d149cfe478a80ce36d3371f19
albinvll <albin.vllahiu@gmail.com>
2020-07-13 10:35:40 +0200

    Reset password css

--- View/forget_password.php
+++ View/forget_password.php
@@ -1,19 +1,23 @@
 <head>
     <link rel="stylesheet" href="../View/css/login.css">
 </head>
-<form>
-    <div class="login-form">
-        <label for="">E-mail</label><br>
-        <input type="text" name="emailForgot" id="emailForgot">
+<body style="background-image:url(Images/photo2-home.jpg); background-size:cover;background-repeat: no-repeat;">
+    <div class="login-content" style="margin:0 auto;padding:30%;">
+        <form>
+        <div class="login-form">
+            <label for="">E-mail</label><br>
+            <input type="text" name="emailForgot" id="emailForgot">
+        </div>
+        <div class="butonat-div">
+            <button onclick="func()" id="forgotButton">
+                Send reset
+            </button>
+        </div>
+        <script>
+            function func() {
+                alert("Thank you, we will send you an email to recover your password if it exists in our system");
+            }
+        </script>
+        </form>
     </div>
-    <div class="butonat-div">
-        <button onclick="func()" id="forgotButton">
-            Login
-        </button>
-    </div>
-    <script>
-        function func() {
-            alert("Thank you, we will send you an email to recover your password if it exists in our system");
-        }
-    </script>
-</form>
+</body>
