c2ecce7d617ab1baa13ce90c60c137f9ebf30c89
dionk6 <kukadion1@gmail.com>
2020-07-15 10:45:15 +0200

    dion commit

--- Model/db_connection.php
+++ Model/db_connection.php
@@ -1,5 +1,5 @@
 <?php
-$connection = mysqli_connect('localhost', 'root', '');
+$connection = mysqli_connect('localhost', 'albin', '');
 if (!$connection){
     die("Database Connection Failed" . mysqli_error($connection));
 }
